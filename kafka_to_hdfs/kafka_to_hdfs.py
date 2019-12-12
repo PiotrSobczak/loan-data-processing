0fae35f0945915fc623c608df658d82c73dbe7b5
PiotrSobczak <piosobc@gmail.com>
2019-12-12 03:33:16 +0100

    Kafka-spark-hive integration

--- kafka_to_hdfs/kafka_to_hdfs.py
+++ kafka_to_hdfs/kafka_to_hdfs.py
@@ -1,14 +1,17 @@
 import findspark
 findspark.init()
 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
+
 import os
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 pyspark-shell' #org.apache.spark:spark-streaming-kafka-0-10_2.11:2.1.0,
-
-from pyspark.sql import SparkSession
 
 spark = SparkSession \
     .builder \
     .appName("SSKafka") \
+    .enableHiveSupport() \
+    .config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
     .getOrCreate()
 
 # default for startingOffsets is "latest", but "earliest" allows rewind for missed alerts
@@ -20,12 +23,20 @@ dsraw = spark \
     .option("startingOffsets", "earliest") \
     .load()
 
-ds = dsraw.selectExpr("CAST(value AS STRING)")
+ds = dsraw.selectExpr("CAST(value AS STRING)").withColumnRenamed("value", "name")
 
-writeQuery = ds \
-        .writeStream \
-        .format("console")\
-        .option("truncate", "false")\
-        .start()
 
+def process_row(row, row_id):
+    row2 = row.withColumn("age", lit(0))
+    row2.write.format('hive').mode("append").saveAsTable('managed_table_new_nt')
+
+
+writeQuery = ds.writeStream.foreachBatch(process_row).start()
+#
+# writeQuery = ds \
+#         .writeStream \
+#         .format("console")\
+#         .option("truncate", "false")\
+#         .start()
+#
 writeQuery.awaitTermination()
